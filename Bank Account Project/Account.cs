399d9984adb4d5edf8d74c3ea895f3bc5db06c56
aduragbemi20 <78370466+aduragbemi20@users.noreply.github.com>
2021-02-02 09:47:46 +0100

    Add files via upload

---
+++ Bank Account Project/Account.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Project
+{
+    class Account
+    {
+        //fields
+        private string firstName;
+        private string lastName;
+
+        private double accountNumber;
+        protected string accountType;
+
+        protected double balance;
+        protected double deposit;
+        protected double withdrawal;
+
+        //properties
+        public string AccountType
+        { get { return this.accountType; } }
+
+        public double Withdrawal
+        {
+            get { return this.withdrawal; }
+            set { this.withdrawal = value; }
+        }
+        public double Deposit
+        {
+            get { return this.deposit; }
+            set { this.deposit = value; }
+        }
+        public double AcctNumber
+        { get { return this.accountNumber; } }
+
+        public double Bal
+        { get { return this.balance; } }
+
+        //constructors
+        public Account()
+        {
+            firstName = "Aduragbemi";
+            lastName = "Sonyaolu";
+        }
+
+        //methods
+        //creates random Account Number
+        public virtual double AccountNumb()
+        {
+            Random rand = new Random();
+            this.accountNumber = rand.Next(100000000, 1000000000);
+            return accountNumber;
+        }
+
+        //Computes General Balance(resets values)
+        public virtual double Balance()
+        {
+            balance = balance + deposit - withdrawal;
+            deposit = 0;
+            withdrawal = 0;
+            return balance;
+        }
+        //Computers Balance when withdrawal equals zero
+        public virtual double DepositBalance(double input)
+        {
+            deposit = input;
+            withdrawal = 0;
+            balance = balance + deposit - withdrawal;
+            return balance;
+        }
+
+        //Computers balance when deposit equals zero
+        public virtual double WithBalance(double input)
+        {
+            withdrawal = input;
+            deposit = 0;
+            balance = balance + deposit - withdrawal;
+            return balance;
+        }
+
+        //displays online banking menu
+        public virtual void DisplayMenu()
+        {
+            Console.WriteLine("OOU Bank Inc.\nWelcome to your online bank account, " + firstName + " " + lastName);
+            Console.WriteLine("Please choose from the options below:\n1.View Client Info\n2.View Account Balance:");
+            Console.WriteLine("     2A.Checking\n     2B.Reserve\n     2C.Savings");
+            Console.WriteLine("3.Deposit Funds:\n     3A.Checking\n     3B.Reserve\n     3C.Savings");
+            Console.WriteLine("4.Withdraw Funds:\n     4A.Checking\n     4B.Reserve\n     4C.Savings");
+            Console.WriteLine("5.Exit");
+        }
+        //client info
+        public string ClientInfo()
+        {
+            string clientinfo = ("Account Holder: " + firstName + " " + lastName);
+            return clientinfo;
+        }
+    }
+}
---
+++ Bank Account Project/Checking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account_Project
+{
+    class Checking : Account
+    {
+        //field
+
+        private double minBalance;
+        private double maxBalance;
+
+
+        //properties
+        public double MinBalance
+        { get { return this.minBalance; } }
+
+        public double MaxBalance
+        { get { return this.maxBalance; } }
+
+
+        //constructors
+
+        public Checking(double balance) : base()
+        {
+            this.minBalance = 500;
+            this.maxBalance = 1000000000000;
+            this.balance = balance;
+            accountType = "Checking Account";
+        }
+
+        //methods
+    }
+}
